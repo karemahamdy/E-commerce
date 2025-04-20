37113bc6d69bea70b1763d16bc422de89e6cb88a
Karema <karemahamdy51@gmail.com>
2025-04-20 22:11:02 +0200

    ..

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <h6 class="bg-red-500">karema</h6>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <a href="https://vite.dev" target="_blank"/>
+  
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
