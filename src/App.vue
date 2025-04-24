7b5aa92992b204cd04e83b93edef185be2dc8df1
Karema <karemahamdy51@gmail.com>
2025-04-24 23:38:38 +0200

    fix: add fucking conflict between tailwind and base e;ement

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div class="wrapper">
-    <div>
-      <Button label="Primary" />
-    </div>
-    <div>
-      <Button label="Secondary" severity="secondary" />
-    </div>
-    <div class="btn-primary">Primary Button</div>
-    <div class="btn-secondary">Secondary Button</div>
 
-    <p class="text-muted">This is muted text.</p>
-    <p class="text-success">This is a success message!</p>
     <p class="text-danger">This is a danger alert.</p>
   </div>
 </template>
