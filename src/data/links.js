3747f9dc76dc12918410585867f2cd2e7f1a1e4c
Karema <karemahamdy51@gmail.com>
2025-04-25 02:29:14 +0300

    '..

---
+++ src/data/links.js
@@ -0,0 +1,7 @@
+export default [
+  { name: "Products", path: "#" },
+  { name: "Who We Are", path: "#" },
+  { name: "Contact Us", path: "#" },
+  { name: "Login", path: "#" },
+  { name: "🛒", path: "#" }
+]
