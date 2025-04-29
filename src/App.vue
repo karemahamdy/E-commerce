8003ac10b44383a730f2d4e234e6932a9a31050d
Karema <karemahamdy51@gmail.com>
2025-04-30 01:01:56 +0300

    .>

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,22 @@
     <TopHeader />
     <MainNavbar />
     <CategoryNavbar />
+    <HeroSection src="../public/assets/images/a9ea64c362124ba0b6ffc37ea30bc5c8.HD-1080p-4.8Mbps-13664080 (1).mp4"
+      />
     <main class="container mx-auto px-4 py-6">
-      
+
     </main>
   </div>
 </template>
 
 <script>
-// Import all components
 import TopHeader from './components/TopHeader.vue';
 import MainNavbar from './components/MainNavbar.vue';
 import CategoryNavbar from './components/CategoryNavbar.vue';
 // import ProductGrid from './components/ProductGrid.vue';
 import './style.css'
 import './tailwind.css'
+import HeroSection from './components/UI/HeroSection.vue';
 
 export default {
   name: 'App',
@@ -24,7 +26,8 @@ export default {
     TopHeader,
     MainNavbar,
     CategoryNavbar,
-    // ProductGrid
+    // ProductGrid,
+    HeroSection
   },
   data() {
     return {
---
+++ src/components/UI/HeroSection.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="relative overflow-hidden" :style="{ '--aspect-ratio': aspectRatio }">
+    <video class="absolute inset-0 w-full h-[30%] object-cover" playsinline muted autoplay preload="metadata"
+      :poster="poster">
+      <source :src="src" type="video/mp4" />
+    </video>
+    <div class="absolute inset-0 bg-black opacity-20 h-[30%] flex justify-center items-center p-4 z[-1]">
+      <div class="text-center">
+        <h2 class="text-3xl text-white">New Collection</h2>
+        <p class="text-lg text-white">Beauties</p>
+      </div>
+      <button
+        class="bg-black text-[#AE9B84] px-5 py-2 rounded-full text-sm hover:bg-black hover:text-white transition absolute bottom-4 left-1/2 -translate-x-1/2"
+        @click="handleClick">
+        {{ buttonText }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroSection",
+  props: {
+    src: String,
+    poster: String,
+    aspectRatio: {
+      type: String,
+      default: "0.75"
+    },
+    buttonText: {
+      type: String,
+      default: "Shop Now"
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleClick() {
+      // يمكنك إضافة أي تعامل هنا عند الضغط على الزر
+      alert("Shop Now clicked!");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.relative::before {
+  content: "";
+  display: block;
+  padding-bottom: calc(100% / var(--aspect-ratio));
+}
+</style>
