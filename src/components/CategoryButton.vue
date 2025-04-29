0f116dffa158a31d40827863904efc6e24995dbf
Karema <karemahamdy51@gmail.com>
2025-04-29 23:10:45 +0300

    restyle color of bttns

--- src/components/CategoryButton.vue
+++ src/components/CategoryButton.vue
@@ -1,8 +1,8 @@
 <!-- CategoryButton.vue -->
 <template>
   <button class="whitespace-nowrap px-4 py-2 text-sm font-medium flex items-center rounded-full transition" :class="isActive
-    ? 'bg-blue-500 text-white'
-    : 'bg-gray-200 text-gray-600 hover:bg-blue-100 hover:text-blue-500'" @click="$emit('click')">
+    ? 'bg-[#c0a27e] text-white'
+  : 'bg-gray-200 text-gray-600 hover:bg-[#AE9B84] hover:text-white'" @click="$emit('click')">
     {{ label }}
     <svg v-if="hasDropdown" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24"
       stroke="currentColor">
