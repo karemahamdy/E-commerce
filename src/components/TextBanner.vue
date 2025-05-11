f2e9e68754475ca1d358fe22950f31c8bca91849
Karema <karemahamdy51@gmail.com>
2025-05-11 23:08:55 +0300

    feat: add text banner

--- src/components/TextBanner.vue
+++ src/components/TextBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-span-1 md:col-span-2 lg:col-span-3">
-    <h1 class="text-2xl font-bold text-center mb-4">{{ title }}</h1>
-    <p class="text-center text-gray-600">{{ description }}</p>
+    <h1 class="text-2xl font-bold text-center mb-4 text-[#c0a27e]">{{ title }}</h1>
+    <p class="text-center text-gray-800">{{ description }}</p>
   </div>
 </template>
 
