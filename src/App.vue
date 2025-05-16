165c95193743458f1dd10a0aae2ea365234a2e81
Karema <karemahamdy51@gmail.com>
2025-05-16 18:38:52 +0300

    add banner and feature section

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@
     <MainNavbar />
     <CategoryNavbar />
     <HeroSection src="../public/assets/images/a9ea64c362124ba0b6ffc37ea30bc5c8.HD-1080p-4.8Mbps-13664080 (1).mp4" />
+    <FeaturesSection/>
     <main class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 pt-8">
       <TextBanner title="Welcome to Our Store" description="Discover the best products at unbeatable prices!" />
       <ProductGrid class="container mx-auto px-4 py-6" />
-      <Category class="container mx-auto px-4 py-6" />
     </main>
+    <Brands class="py-6 mb-4" />
     <SaleBanner />
-    <Brands class="container mx-auto px-4 py-6" />
+    <!-- <Category class="container mx-auto px-4 py-6" /> -->
     <Footer />
 
   </div>
@@ -29,6 +30,7 @@ import Footer from './components/UI/Footer.vue';
 import Category from './components/UI/Category.vue';
 import SaleBanner from './components/UI/SaleBanner.vue';
 import Brands from './components/UI/Brands.vue';
+import FeaturesSection from './components/FeaturesSection.vue';
 
 export default {
   name: 'App',
@@ -42,6 +44,7 @@ export default {
     Category,
     SaleBanner,
     Brands,
+    FeaturesSection,
     Footer
   },
   data() {
--- src/components/UI/SaleBanner.vue
+++ src/components/UI/SaleBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mb-6">
-    <img src="../../../public/assets/images/saleBanner.png" alt="Sale Banner"
+    <img src="../../../public/assets/images/bannerSale.webp" alt="Sale Banner"
       class="rounded shadow w-full" />
   </div>
 </template>
