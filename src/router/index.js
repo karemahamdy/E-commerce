9601d9a7d345c7b2c20f65724d6c49c29e52880c
Karema <karemahamdy51@gmail.com>
2025-05-17 00:51:28 +0300

    sdd product

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home }
+  { path: '/', name: 'Home', component: Home },
+  { path: '/home', name: 'Home', component: Home },
+  { path: '/product', name: 'Product', component: Product }
+
 ]
 
 const router = createRouter({
