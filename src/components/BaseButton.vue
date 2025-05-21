c2e6da1a85e9990c25edb3039ac664bdd3af7e75
karemahamdy <karemahamdy51@gmail.com>
2025-05-21 15:59:54 +0300

    feat:  add base button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,61 @@
+<template>
+  <component
+    :is="to ? 'router-link' : 'button'"
+    :to="to"
+    :class="[
+      bgColor,
+      textColor,
+      hoverBgColor,
+      rounded,
+      padding,
+      textSize,
+      'transition',
+    ]"
+    @click="handleClick"
+  >
+    {{ label }}
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    to: {
+      type: String,
+      default: null
+    },
+    bgColor: {
+      type: String,  
+    },
+    textColor: {
+      type: String,
+    },
+    hoverBgColor: {
+      type: String,
+    },
+      hoverTextColor: {
+      type: String,
+      default: 'hover:text-black',
+    },
+    rounded: {
+      type: String,
+    },
+    padding: {
+      type: String,  
+    },
+    textSize: {
+      type: String,    
+    }
+  },
+  computed: {
+    computedClasses() {
+      return `${this.bgColor} ${this.textColor} ${this.hoverBgColor}  ${this.padding} ${this.rounded} ${this.textSize} ${this.hoverTextColor}`
+    }
+  }
+}
+</script>
--- src/components/UI/HeroSection.vue
+++ src/components/UI/HeroSection.vue
@@ -4,26 +4,21 @@
       :poster="poster">
       <source :src="src" type="video/mp4" />
     </video>
-
-    <!-- Black overlay -->
     <div class="absolute inset-0 bg-black opacity-50 z-10"></div>
-
-    <!-- Content over video and overlay -->
     <div class="absolute inset-0 flex flex-col justify-center items-center p-4 z-20">
       <div class="text-center">
         <h2 class="text-4xl text-white mb-2">New Collection</h2>
         <p class="text-xl text-white mb-6">Beauties</p>
-        <button
-          class="bg-black text-[#AE9B84] px-6 py-3 rounded-full text-sm hover:bg-black hover:text-white transition"
-          @click="handleClick">
-          {{ buttonText }}
-        </button>
+        <BaseButton label="Add to Cart" bgColor="bg-black" textColor="text-white" hoverBgColor="hover:bg-[#AE9B84]"
+          rounded="rounded-full" padding="px-8 py-[1rem]" textSize="text-base" to="/product" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import BaseButton from '../BaseButton.vue';
+
 export default {
   name: "HeroSection",
   props: {
@@ -34,6 +29,7 @@ export default {
       default: "Shop Now"
     }
   },
+  components: { BaseButton },
   methods: {
     handleClick() {
       alert("Shop Now clicked!");
