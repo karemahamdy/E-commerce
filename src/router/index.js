9a900dc7abc3ae00a000f1560f2533b1aa017be2
karemahamdy <karemahamdy51@gmail.com>
2025-05-21 12:50:33 +0300

    ..

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Product from '../views/Product.vue'
 import Cart from '../views/Cart.vue'
 import Summary from '../views/Summary.vue'
 import Products from '../views/Products.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
@@ -11,6 +12,8 @@ const routes = [
   { path: '/product', name: 'Product', component: Products },
   { path: '/cart', name: 'Cart', component: Cart },
   { path: '/summary', name: 'Summary', component: Summary },
+  { path: '/login', name: 'login', component: Login },
+
 ]
 
 const router = createRouter({
