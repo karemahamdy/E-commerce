5513c7e59441dc2dd5801feef2727d3fc5680b3e
karemahamdy <karemahamdy51@gmail.com>
2025-05-22 14:22:41 +0300

    refactor : summary order

---
+++ src/components/layout/Checkout/PaymentWay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="space-y-2 text-sm text-gray-700">
+        <div>
+          <Checkbox v-model="form.payment" inputId="check" value="check" />
+          <label for="check" class="ml-2">Cah Payment</label>
+        </div>
+        <div>
+          <Checkbox v-model="form.payment" inputId="paypal" value="paypal" />
+          <label for="paypal" class="ml-2">Paypal</label>
+        </div>
+      </div>
+</template>
+
+<script>
+
+import Checkbox from 'primevue/checkbox'
+import Button from 'primevue/button'
+
+
+export default {
+  components: {  
+    Checkbox,
+    Button,
+  },
+  data() {
+    return {
+      form: {
+        createAccount: false,
+        payment: null
+      }
+    }
+  }
+}
+</script>
